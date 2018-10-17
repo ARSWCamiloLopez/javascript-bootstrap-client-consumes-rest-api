a4b0266f70ae9318ac38c643035540085e7ba49c
Camilo Lopez <camilino-123@hotmail.com>
2018-10-17 13:24:23 -0500

    Laboratorio terminado

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -1,103 +1,69 @@
-var order1 = {
-	"order_id": 1,
-	"table_id": 1,
-	"products": [{
-			"product": "Pizza",
-			"quantity": 3,
-			"price": "$10000"
-		},
-		{
-			"product": "Hot dog",
-			"quantity": 1,
-			"price": "$3000"
-		},
-		{
-			"product": "Coke",
-			"quantity": 4,
-			"price": "$1300"
-		}
-	]
-}
-
-var order2 = {
-	"order_id": 2,
-	"table_id": 2,
-	"products": [{
-			"product": "Pizza",
-			"quantity": 3,
-			"price": "$15000"
-		},
-		{
-			"product": "Hamburguer",
-			"quantity": 1,
-			"price": "$12300"
-		}
-	]
-}
-
-var order3 = {
-	"order_id": 3,
-	"table_id": 3,
-	"products": [{
-			"product": "Pizza",
-			"quantity": 5,
-			"price": "$25000"
-		},
-		{
-			"product": "Hamburguer",
-			"quantity": 2,
-			"price": "$24600"
-		},
-		{
-			"product": "Soda",
-			"quantity": 3,
-			"price": "$4600"
-		}
-	]
-}
-
-function loadOrders(){
-	addTableToPage(order1);
-	addTableToPage(order2);
-	addTableToPage(order3);
-}
-
 var firstRow = ["Product", "Quantity", "Price"];
 
 function addTableToPage(order){
 
-	var main = document.getElementById("main");
-	var table = document.createElement("table");
+    var main = document.getElementById("main");
+    var table = document.createElement("table");
 
-	table.setAttribute("class", "table");
-	table.setAttribute("id", order.order_id);
+    table.setAttribute("class", "table");
+    table.setAttribute("id", order.order_id);
 
-	row = document.createElement("tr");
+    row = document.createElement("tr");
 
-	for(var x in firstRow){
-		var rowItem = document.createElement("th");
-		rowItem.innerHTML = firstRow[x];
-		row.appendChild(rowItem);
-	}
+    for(var x in firstRow){
+        var rowItem = document.createElement("th");
+        rowItem.innerHTML = firstRow[x];
+        row.appendChild(rowItem);
+    }
 
-	table.appendChild(row);
+    table.appendChild(row);
 
-	for(var key in order.products){
-		var row = document.createElement("tr");
-		for(var element in order.products[key]){
-			var rowItem = document.createElement("td");
-			rowItem.innerHTML = order.products[key][element];
-			row.appendChild(rowItem);
-		}
-		table.appendChild(row);
-	}
-	main.appendChild(table);
+    for(var key in order.products){
+        var row = document.createElement("tr");
+        for(var element in order.products[key]){
+                var rowItem = document.createElement("td");
+                rowItem.innerHTML = order.products[key][element];
+                row.appendChild(rowItem);
+        }
+        table.appendChild(row);
+    }
+    main.appendChild(table);
 }
 
 function removeOrderById(idOrder){
 	document.getElementById(idOrder).remove();
 }
 
-function tryingAxios(){
-	axios("https://localhost:8080/orders");
+function loadOrders(){
+    var ordenes = [];
+    axios.get('/orders')
+        .then(function(response){
+            orders = response.data;
+            for(var i in orders){
+                var order = {	"order_id": 0,
+                                "table_id": 0,
+                                "products": []};
+                order.order_id = Number(i);
+                for(var j in orders[i]){
+                    if(j == "tableNumber"){
+                        order.table_id = orders[i][j];                        
+                    }else{
+                        for(var k in orders[i][j]){
+                            var infoTable = {};
+                            infoTable["product"] = k;
+                            infoTable["quantity"] = orders[i][j][k];
+                            infoTable["price"] = "$10000";
+                            order["products"].push(infoTable);
+                        }                        
+                    }
+                }
+                ordenes.push(order);
+            }
+            for(var x in ordenes){
+                addTableToPage(ordenes[x]);
+            }
+        })
+        .catch(function (error) {
+            console.log("There is a problem with our servers. We apologize for the inconvince, please try again later" + error);
+        });
 }
